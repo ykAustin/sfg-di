28adb8aab618afaf1b9f6a5eea511a7fa045dd12
awu <austin.ykwu@gmail.com>
2023-03-05 22:48:52 +0800

    update

---
+++ src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
@@ -0,0 +1,20 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by jt on 12/26/19.
+ */
+@Controller
+public class ConstructorInjectedController {
+    private final GreetingService greetingService;
+
+    public ConstructorInjectedController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String getGreeting(){
+        return greetingService.sayGreeting();
+    }
+}
---
+++ src/main/java/guru/springframework/sfgdi/services/GreetingServiceImpl.java
@@ -0,0 +1,16 @@
+package guru.springframework.sfgdi.services;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by jt on 12/26/19.
+ */
+@Primary
+@Service
+public class GreetingServiceImpl implements GreetingService {
+    @Override
+    public String sayGreeting() {
+        return "Hello World - From the PRIMARY";
+    }
+}
